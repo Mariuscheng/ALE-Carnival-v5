5eb13e43fe935c9503fe8ded7feb7f5feabc2131
Mariuscheng <47233413+Mariuscheng@users.noreply.github.com>
2024-08-04 15:57:06 +0800

    Update personal_ai.py

--- personal_ai.py
+++ personal_ai.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import ollama
 
-st.write("Hello World")
+def main():
+  st.title("My personal A.I")
+
+  user_input = st.text_area("What do you want to ask?","")
+
+  if st.button("SEND")
+    if user_input:
+      response = ollama.chat(model='mistral', message=[{'role': 'user', 'content': user_input}])
+
+      st.text("REPLY :")
+      st.write(response['message']['content'])
+    else:
+      st.warning("please ask question !")
+
+if __name__ == "__main__":
+  main()
